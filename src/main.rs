4da63c1cc9652b4b8af38a4faf5f1dd8c07cc42b
mehtaishita <ishita.mehta33@outlook.com>
2023-06-29 12:18:52 -0500

    request adapter - native gpu Metal
    complaining about the constant though

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use gfx_hal::Instance;
+use std::borrow::Cow;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
+
+pub async fn run(event_loop: EventLoop<()>, window: Window) {
+    let surface = unsafe { instance.create_surface(&window) };
+    let instance = wgpu::Instance::new(wgpu::Backends::METAL);
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        })
+        .await
+        .expect("Failed to find appropriate adapter");
+}
 
 fn main() {
     let event_loop = EventLoop::new();
